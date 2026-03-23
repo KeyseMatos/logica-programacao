736fc1297f38399756928340e89ff11c2141aa01
KeyseMatos <Keysematos86@gmail.com>
2026-03-23 20:56:39 -0300

    aula 04 vetor

---
+++ aula04/exerc01.js
@@ -0,0 +1,7 @@
+var num = [11, 2, 3]
+
+if (num[0] > 10) {
+    console.log("Acesso direto ao vetor")
+} else if (num[0] < 10) {
+    console.log("Tente novamente")
+}
---
+++ aula04/vetor.js
@@ -0,0 +1,8 @@
+var num = [1, 2, 3, 4, 5]
+
+console.log(num[0])
+console.log(num[1])
+console.log(num[2])
+console.log(num[3])
+console.log(num[4])
+console.log(num[5])
