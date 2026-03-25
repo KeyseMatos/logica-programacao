5388d2d05b46e1467924ac267ce4c5b936e14aba
KeyseMatos <Keysematos86@gmail.com>
2026-03-25 19:50:01 -0300

    Aula 05 atividade sobre vetores

---
+++ aula05/att02.js
@@ -0,0 +1,9 @@
+//2. Dado o vetor [15, 5, 8], verifique se o valor na posição 1 é maior do que 10, se sim, retornar "Verdadeiro", se não, retornar "Falso".
+
+var num = [15, 5, 8]
+
+if (num[1] > 10) { //verifica se o primeiro número é maior que 10
+    console.log("Verdadeiro")
+} else {
+    console.log("Falso") //senão for exibir falso
+}
