42cbc715327d56f37d478bd4341f7becc2e7fcd8
KeyseMatos <152615835+KeyseMatos@users.noreply.github.com>
2026-04-08 21:29:14 -0300

    Aula 08 - While e For

---
+++ quest01.js
@@ -0,0 +1,12 @@
+//QUESTÃO 1 - Escrever um programa que leia a idade de 7 pessoas e no final diga quantas pessoas são maiores de idade (>= 18 anos). 
+const prompt = require("prompt-sync")();
+
+let maiores = 0;
+
+for(let i = 0; i< 7; i++){
+    let idade = parseInt(prompt("Digite a idade da pessoa " + i + ": "));
+    if(idade >= 18){
+        maiores++;
+    }
+}
+console.log("Quantidade de pessoas maiores de idade: ", maiores);
